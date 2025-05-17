79aab5a1064af7439bf03e09122512d1415b4782
JoelCota <92892169+JoelCota@users.noreply.github.com>
2025-05-17 11:52:02 -0700

    Clean Files

--- app/invernadero/src/main/java/com/itson/invernadero/service/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.itson.invernadero.service;
-
-import com.itson.invernadero.model.Usuario;
-
-public interface UsuarioService {
-    Usuario registrarUsuario(Usuario usuario);
-    Usuario buscarPorEmail(String email);
-}
-
